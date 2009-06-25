c49cf7b576793714003f441309b96f6d960e7ffe
Mike Judge <mike@activerain.com>
2009-06-25 15:30:33 -0700

    Changed method of monkeypatching to an extend rather than a require, which seems to lose it when a reload happens

--- init.rb
+++ init.rb
@@ -1,2 +1,2 @@
-require File.join(File.dirname(__FILE__), 'lib', 'paperclip_validations')
+Paperclip::ClassMethods.send(:extend, PaperclipValidations)
 
--- lib/paperclip_validations.rb
+++ lib/paperclip_validations.rb
@@ -1,43 +1,41 @@
-module Paperclip
-  module ClassMethods
-    # Places ActiveRecord-style validations on the ratio of the dimensions of the file assigned.
-    # 
-    #   # restricts the image from a perfectly square shape to 16:9 widescreen.
-    #   validates_ratio_of :image, :with => (1.0)..(1.78)
-    #
-    def validates_ratio_of name, options={}
-      attachment_definitions[name][:validations][:dimensions] = lambda do |attachment, instance|
-        path = instance.send(name).queued_for_write[:original]
-        if path
-          dimensions       = Paperclip::Geometry.from_file path
-          ratio_dimensions = [dimensions.width, dimensions.height].sort.reverse
-          ratio            = ratio_dimensions.first / ratio_dimensions.last.to_f
+module PaperclipValidations
+  # Places ActiveRecord-style validations on the ratio of the dimensions of the file assigned.
+  # 
+  #   # restricts the image from a perfectly square shape to 16:9 widescreen.
+  #   validates_ratio_of :image, :with => (1.0)..(1.78)
+  #
+  def validates_ratio_of name, options={}
+    attachment_definitions[name][:validations][:dimensions] = lambda do |attachment, instance|
+      path = instance.send(name).queued_for_write[:original]
+      if path
+        dimensions       = Paperclip::Geometry.from_file path
+        ratio_dimensions = [dimensions.width, dimensions.height].sort.reverse
+        ratio            = ratio_dimensions.first / ratio_dimensions.last.to_f
 
-          message = options[:message] || "ratio must be #{options[:width].first} to #{options[:width].second}"
-          message unless options[:with].include? ratio
-         end
+        message = options[:message] || "ratio must be #{options[:width].first} to #{options[:width].second}"
+        message unless options[:with].include? ratio
       end
     end
+  end
 
-    # Places ActiveRecord-style validations on the dimensions of the file assigned.  The
-    # possible options are:
-    #
-    # * +width+: A width range or integer (e.g., 1..200, 200)
-    # * +height+: A height range or integer (e.g., 1..200, 200)
-    # * +message+: An error message overriding the default
-    #
-    #   validates_dimensions_of :image, :width => 125..1024, :height => 125..1024
-    #
-    def validates_dimensions_of name, options = {}
-      attachment_definitions[name][:validations][:dimensions] = lambda do |attachment, instance|
-        path = instance.send(name).queued_for_write[:original]
-        if path
-          dimensions = Paperclip::Geometry.from_file path
-          message    = options[:message] || "dimensions must be #{options[:width]}px wide by #{options[:height]}px tall"
-   
-          message unless (options[:width].present? and options[:width] === dimensions.width) or
-                         (options[:height].present? and options[:height] === dimensions.height)
-        end
+  # Places ActiveRecord-style validations on the dimensions of the file assigned.  The
+  # possible options are:
+  #
+  # * +width+: A width range or integer (e.g., 1..200, 200)
+  # * +height+: A height range or integer (e.g., 1..200, 200)
+  # * +message+: An error message overriding the default
+  #
+  #   validates_dimensions_of :image, :width => 125..1024, :height => 125..1024
+  #
+  def validates_dimensions_of name, options = {}
+    attachment_definitions[name][:validations][:dimensions] = lambda do |attachment, instance|
+      path = instance.send(name).queued_for_write[:original]
+      if path
+        dimensions = Paperclip::Geometry.from_file path
+        message    = options[:message] || "dimensions must be #{options[:width]}px wide by #{options[:height]}px tall"
+
+        message unless (options[:width].present? and options[:width] === dimensions.width) or
+        (options[:height].present? and options[:height] === dimensions.height)
       end
     end
   end
