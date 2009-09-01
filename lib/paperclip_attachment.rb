b7972f6601569e58afb7814735c5818f67e9092a
Mike Judge <mike@activerain.com>
2009-09-01 12:30:50 -0700

    Updated the plugin to work with the latest version of paperclip

---
+++ lib/paperclip_attachment.rb
@@ -0,0 +1,26 @@
+module Paperclip
+  class Attachment
+    def validate_ratio options
+      if @queued_for_write[:original].present?
+        dimensions       = Paperclip::Geometry.from_file @queued_for_write[:original]
+        ratio_dimensions = [dimensions.width, dimensions.height].sort.reverse
+        ratio            = ratio_dimensions.first / ratio_dimensions.last.to_f
+
+        message = options[:message] || "ratio must be #{options[:with].first} to #{options[:with].last}"
+        message unless options[:with].include? ratio
+      end
+    end
+
+    def validate_dimensions options
+      if @queued_for_write[:original].present?
+        dimensions = Paperclip::Geometry.from_file @queued_for_write[:original]
+        message    = options[:message] || "dimensions must be #{options[:width]}px wide by #{options[:height]}px tall"
+
+        message unless (options[:width].present? and options[:width] === dimensions.width) or
+        (options[:height].present? and options[:height] === dimensions.height)
+      end
+    end
+  end
+end
+
+
--- lib/paperclip_validations.rb
+++ lib/paperclip_validations.rb
@@ -1,3 +1,4 @@
+
 module PaperclipValidations
   # Places ActiveRecord-style validations on the ratio of the dimensions of the file assigned.
   # 
@@ -5,17 +6,7 @@ module PaperclipValidations
   #   validates_ratio_of :image, :with => (1.0)..(1.78)
   #
   def validates_ratio_of name, options={}
-    attachment_definitions[name][:validations][:ratio] = lambda do |attachment, instance|
-      path = instance.send(name).queued_for_write[:original]
-      if path
-        dimensions       = Paperclip::Geometry.from_file path
-        ratio_dimensions = [dimensions.width, dimensions.height].sort.reverse
-        ratio            = ratio_dimensions.first / ratio_dimensions.last.to_f
-
-        message = options[:message] || "ratio must be #{options[:with].first} to #{options[:with].last}"
-        message unless options[:with].include? ratio
-      end
-    end
+    attachment_definitions[name][:validations] << [:ratio, options]
   end
 
   # Places ActiveRecord-style validations on the dimensions of the file assigned.  The
@@ -28,15 +19,6 @@ module PaperclipValidations
   #   validates_dimensions_of :image, :width => 125..1024, :height => 125..1024
   #
   def validates_dimensions_of name, options = {}
-    attachment_definitions[name][:validations][:dimensions] = lambda do |attachment, instance|
-      path = instance.send(name).queued_for_write[:original]
-      if path
-        dimensions = Paperclip::Geometry.from_file path
-        message    = options[:message] || "dimensions must be #{options[:width]}px wide by #{options[:height]}px tall"
-
-        message unless (options[:width].present? and options[:width] === dimensions.width) or
-        (options[:height].present? and options[:height] === dimensions.height)
-      end
-    end
+    attachment_definitions[name][:validations] << [:dimensions, options]
   end
 end
